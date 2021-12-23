675c831e1b3b9f9c84b746e7f2a53f3248112b72
FranPandol <pandol.francisco@gmail.com>
2021-12-23 09:44:35 -0300

    Run devicefarm using config file from env

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,6 @@
 
 mkdir /tmp/apptim-cli
 curl --output /tmp/apptim-cli/cli.zip https://devicefarm.apptim.com/apptim-devicefarm-ubuntu-20.04-1.7.3dev.zip
-pwd
-ls
 unzip /tmp/apptim-cli/cli.zip -d /tmp/apptim-cli
 
-/tmp/apptim-cli/devicefarm run --config config.yml
+/tmp/apptim-cli/devicefarm run --config ${CONFIG_PATH}
