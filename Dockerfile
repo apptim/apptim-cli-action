1b7dd9090cc66f5681b680c2f0cb6b987863e435
FranPandol <pandol.francisco@gmail.com>
2021-12-23 15:02:53 -0300

    Change base image to ubuntu 20.04. Delete extra steps

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,7 @@
-FROM python:3.8
+FROM ubuntu:20.04
 
 WORKDIR /app
-
-LABEL "com.github.actions.name"="apptim-cli-action"
-LABEL "com.github.actions.description"="Run test in device farm"
-LABEL "com.github.actions.icon"="refresh-cw"
-LABEL "com.github.actions.color"="green"
-
-LABEL version="0.0.1"
-LABEL repository="https://github.com/apptim/apptim-cli-action"
-LABEL homepage="https://apptim.com"
-LABEL maintainer="Apptim Dev Team <support@apptim.com>"
-
-# https://github.com/aws/aws-cli/blob/master/CHANGELOG.rst
-ENV AWSCLI_VERSION='1.18.14'
-
-RUN pip3 install --quiet --no-cache-dir awscli==${AWSCLI_VERSION}
-
-#ADD install-packages.sh /install-packages.sh
-#RUN ./install-packages.sh
-RUN apt-get update
+RUN apt-get -y update
 RUN apt-get -y install curl unzip wget
 
 ADD entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,44 +1,7 @@
 #!/bin/bash
 
-if [ -z "$AWS_ACCESS_KEY_ID" ]; then
-  echo "AWS_ACCESS_KEY_ID is not set. Quitting."
-  exit 1
-fi
-
-if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
-  exit 1
-fi
-
-# Default to us-east-1 if AWS_REGION not set.
-if [ -z "$AWS_REGION" ]; then
-  AWS_REGION="us-east-1"
-fi
-
-
-# Create a dedicated profile for this action to avoid conflicts
-# with past/future actions.
-# https://github.com/jakejarvis/s3-sync-action/issues/1
-aws configure --profile apptim-cli-action-profile <<-EOF > /dev/null 2>&1
-${AWS_ACCESS_KEY_ID}
-${AWS_SECRET_ACCESS_KEY}
-${AWS_REGION}
-text
-EOF
-
 mkdir /tmp/apptim-cli
 curl --output /tmp/apptim-cli/cli.zip https://devicefarm.apptim.com/apptim-devicefarm-ubuntu-20.04-1.7.3dev.zip
 unzip /tmp/apptim-cli/cli.zip -d /tmp/apptim-cli
 
 /tmp/apptim-cli/devicefarm run --config ${CONFIG_PATH}
-
-# Clear out credentials after we're done.
-# We need to re-run `aws configure` with bogus input instead of
-# deleting ~/.aws in case there are other credentials living there.
-# https://forums.aws.amazon.com/thread.jspa?threadID=148833
-aws configure --profile apptim-cli-action-profile <<-EOF > /dev/null 2>&1
-null
-null
-null
-text
-EOF
