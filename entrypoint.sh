b0b10610cbf9fbc801132e06eeffeef8a5faf123
Fabian <fbaptista@users.noreply.github.com>
2022-01-11 17:14:12 -0300

    Use apptim-cli v1.8

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir /tmp/apptim-cli
-curl --output /tmp/apptim-cli/cli.zip https://devicefarm.apptim.com/apptim-devicefarm-ubuntu-20.04-1.7.3dev.zip
+curl --output /tmp/apptim-cli/cli.zip https://devicefarm.apptim.com/apptim-devicefarm-ubuntu-20.04-1.8.zip
 unzip /tmp/apptim-cli/cli.zip -d /tmp/apptim-cli
 
 /tmp/apptim-cli/devicefarm run --config ${CONFIG_PATH}
