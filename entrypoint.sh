9346d4455f1e1cc5538b611bf29747143e50586f
franpandol <pandol.fancisco@gmail.com>
2022-02-09 12:10:45 -0300

    AV-150: Add default for APPTIM_CLI_VERSION. Add optional param APPTIM_AGENT_VERSION.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+ # Default to 1.11 if APPTIM_CLI_VERSION not set.
+if [ -z "$APPTIM_CLI_VERSION" ]; then
+	APPTIM_CLI_VERSION=1.11
+fi
+
 mkdir /tmp/apptim-cli
-curl --output /tmp/apptim-cli/cli.zip https://devicefarm.apptim.com/apptim-devicefarm-ubuntu-20.04-${APPTIM_CLI_VERSION}.zip
+curl --output /tmp/apptim-cli/cli.zip https://devicefarm.apptim.com/apptim-cli-ubuntu-20.04-${APPTIM_CLI_VERSION}.zip
 unzip /tmp/apptim-cli/cli.zip -d /tmp/apptim-cli
 
-/tmp/apptim-cli/devicefarm run --config ${CONFIG_PATH}
+if [ -n "$APPTIM_AGENT_VERSION" ]; then
+	/tmp/apptim-cli/apptim run --config ${CONFIG_PATH}  --agent-version ${APPTIM_AGENT_VERSION}
+else
+	/tmp/apptim-cli/apptim run --config ${CONFIG_PATH}
+fi
+
+
