ed44005896e6a50e85e74664af495a47c0d26158
Fabian <fbaptista@users.noreply.github.com>
2022-09-21 11:45:44 -0300

    Use latest apptim-cli (v1.19)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
- # Default to 1.18 if APPTIM_CLI_VERSION not set.
+ # Default to 1.19 if APPTIM_CLI_VERSION not set.
 if [ -z "$APPTIM_CLI_VERSION" ]; then
-	APPTIM_CLI_VERSION=1.18
+	APPTIM_CLI_VERSION=1.19
 fi
 
 mkdir /tmp/apptim-cli
