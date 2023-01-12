246c50fb839f7bd618c4b0c285f8bc6594759b90
Juan <aguseme01@outlook.com>
2023-01-12 12:20:01 -0300

    Use latest apptim-cli (v1.20.3)
    Set new expiration dates
    Use agent v0.9.4.1 (timeout fix)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
- # Default to 1.19 if APPTIM_CLI_VERSION not set.
+ # Default to 1.20.3 if APPTIM_CLI_VERSION not set.
 if [ -z "$APPTIM_CLI_VERSION" ]; then
-	APPTIM_CLI_VERSION=1.19
+	APPTIM_CLI_VERSION=1.20.3
 fi
 
 mkdir /tmp/apptim-cli
